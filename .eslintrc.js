0b3fab01b682c0debf56c745243f8ea8bb45d9ec
jimme <hendrixjimme@gmail.com>
2022-04-04 13:04:01 +0200

    Set everything up

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	'env': {
+		'es2021': true,
+		'node': true,
+		'mocha': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
---
+++ test/example.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+describe('An example test', () => {
+	it('Tests the code', () => {
+		const x = 'y';
+		expect(x).to.equal('y');
+	});
+});
